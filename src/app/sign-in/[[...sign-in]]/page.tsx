85a10b61ec0b52542f8715561c44159bafe3c9b7
Jillian Breau <jmbreau@mun.ca>
2024-10-01 18:47:36 -0230

    add link to unauthenticated from sign-in

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,9 +1,13 @@
+import Link from 'next/link'
 import { SignIn } from '@clerk/nextjs'
 
 export default function Page() {
   return (
-    <div className='flex w-full justify-center'>
+    <div className='flex flex-col w-full items-center space-y-6'>
       <SignIn />
+      <div>
+        Not an admin? Check out the public <Link href="/">resources page</Link>.
+      </div>
     </div>
   )
 }

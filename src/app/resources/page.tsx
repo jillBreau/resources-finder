746ef6d70204e23d27b5b42e4bb88f305422cb16
Jillian Breau <jmbreau@mun.ca>
2024-10-01 18:47:44 -0230

    initial supabase setup

--- src/app/resources/page.tsx
+++ src/app/resources/page.tsx
@@ -1,7 +1,13 @@
-export default function Page() {
+import { createClient } from '@/utils/supabase/server';
+
+export default async function Page() {
+  const supabase = createClient();
+  const { data: notes } = await supabase.from("notes").select();
+
   return (
-    <div className="flex items-center">
-      Resources page
+    <div className="flex flex-col items-start justify-start space-y-4">
+      <span>Resources page</span>
+      <pre>{JSON.stringify(notes, null, 2)}</pre>
     </div>
   );
 }

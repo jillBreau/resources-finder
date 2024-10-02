f4ed0a32995c6a9a4d1e23c4ca40a7ed958b3a39
Jillian Breau <jmbreau@mun.ca>
2024-10-01 22:28:23 -0230

    fetch reviewed non-deprecated resources

--- src/app/resources/page.tsx
+++ src/app/resources/page.tsx
@@ -1,13 +1,77 @@
+import { Resource } from '@/types/resources';
 import { createClient } from '@/utils/supabase/server';
 
 export default async function Page() {
   const supabase = createClient();
-  const { data: notes } = await supabase.from("notes").select();
+  const { data: resources } = await supabase
+    .from('resources')
+    .select()
+    .eq('reviewed', true)
+    .eq('deprecated', false)
 
   return (
-    <div className="flex flex-col items-start justify-start space-y-4">
-      <span>Resources page</span>
-      <pre>{JSON.stringify(notes, null, 2)}</pre>
+    <div className="flex flex-col items-start justify-start space-y-16">
+      {resources?.map(({
+        id,
+        name,
+        email, 
+        websiteUrl,
+        slackUrl,
+        discordUrl,
+        instagramUrl,
+        facebookUrl,
+        twitterUrl,
+        linkedinUrl,
+        youtubeUrl,
+        description,
+        participantDemographics,
+        volunteerDemographics,
+        region,
+        formats,
+        categories,
+      }: Resource) => (
+        <div className="flex flex-col space-y-8" key={id}>
+          <span>{name}</span>
+          <div className="flex space-x-2">
+            <span>{email}</span>
+            <span>{websiteUrl}</span>
+            <span>{slackUrl}</span>
+            <span>{discordUrl}</span>
+          </div>
+          <div className="flex space-x-2">
+            <span>{instagramUrl}</span>
+            <span>{facebookUrl}</span>
+            <span>{twitterUrl}</span>
+            <span>{linkedinUrl}</span>
+            <span>{youtubeUrl}</span>
+          </div>
+          <span>{description}</span>
+          <div>
+            {participantDemographics?.map((participantDemographic) => (
+              <span>{`(${participantDemographic}) `}</span>
+            ))}
+          </div>
+          <div>
+            {volunteerDemographics?.map((volunteerDemographic) => (
+              <span>{`[${volunteerDemographic}] `}</span>
+            ))}
+          </div>
+          <div className="flex space-x-2">
+            <span>{region}</span>
+            <div>
+              {formats?.map((format) => (
+                <span>{`(${format}) `}</span>
+              ))}
+            </div>
+          </div>
+          <div>
+            {categories?.map((category) => (
+              <span>{`[${category}] `}</span>
+            ))}
+          </div>
+          <span>----</span>
+        </div>
+      ))}
     </div>
   );
 }
---
+++ src/types/resources.ts
@@ -0,0 +1,94 @@
+const PERSON_AGES = [
+  'Child',
+  'Teen',
+  'Adult',
+]
+
+const PERSON_OCCUPATIONS = [
+  'Post-secondary student',
+  'Early-career professional', 
+  'Mid-career professional',
+  'Late-career professional',
+]
+
+const PERSON_GENDERS = [
+  'Female',
+  'Non-binary',
+  'Male',
+]
+
+const PERSON_COMMUNITIES = [
+  'LGBTQIA2S+',
+  'Not a member of the above community',
+]
+
+const PERSON_GROUPS = [
+  'Black',
+  'Indigenous',
+  'Not a member of the above groups',
+]
+
+const PERSON_DEMOGRAPHICS = [
+  ...PERSON_AGES,
+  ...PERSON_OCCUPATIONS,
+  ...PERSON_GENDERS,
+  ...PERSON_COMMUNITIES,
+  ...PERSON_GROUPS,
+] as const
+
+type PersonDemographic = typeof PERSON_DEMOGRAPHICS[number]
+type PersonDemographics = PersonDemographic[]
+
+const REGIONS = [
+  'NL',
+  'Atlantic Canada',
+  'Canada',
+  'Global',
+] as const
+
+type Region = typeof REGIONS[number]
+
+const FORMATS = [
+  'Virtual',
+  'In-person',
+] as const
+
+type Format = typeof FORMATS[number]
+type Formats = Format[]
+
+const CATEGORIES = [
+  'Tech education',
+  'Tech community',
+  'Tech mentorship',
+  'General mentorship',
+] as const
+
+type Category = typeof CATEGORIES[number]
+type Categories = Category[]
+
+export type Resource = {
+  id?: string
+  createdAt?: string
+  name: string
+  email?: string 
+  websiteUrl?: string
+  slackUrl?: string
+  discordUrl?: string
+  instagramUrl?: string
+  facebookUrl?: string
+  twitterUrl?: string
+  linkedinUrl?: string
+  youtubeUrl?: string
+  description: string
+  participantDemographics: PersonDemographics
+  volunteerDemographics: PersonDemographics
+  region: Region
+  formats: Formats
+  categories: Categories
+}
+
+type NewResource = Resource & {
+  reviewed: boolean
+  deprecated: boolean
+  submittedBy?: string
+}
